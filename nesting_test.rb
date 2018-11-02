e28b8d38b12e4ad33b0992c5f0b72c2028a9a64b
zdcthomas <zdcthomas@yahoo.com>
2018-11-02 12:01:34 -0600

    adds example test

--- nesting_test.rb
+++ nesting_test.rb
@@ -4,6 +4,16 @@ require 'minitest/pride'
 
 class NestedTest < MiniTest::Test
 
+  def test_list_of_olive_garden_employess
+    skip
+    #^^^ Un-Skip each test
+    #=======================
+    # EXAMPLE
+    employees = stores[:olive_garden][:employees]
+    #=======================
+    assert_equal ["Jeff", "Zach", "Samantha"], employees
+  end
+
   def test_pancake_ingrediants
     skip
     #=======================
